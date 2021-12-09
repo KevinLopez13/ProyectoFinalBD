3a1ec99783b5235ecbb772eecab9ba167df8f024
KevinLopez13 <enanodeoz@gmail.com>
2021-12-09 16:05:52 -0600

    Se agregaron mensajes de errores.

--- BaseDeDatos/creacion_tablas.sql
+++ BaseDeDatos/creacion_tablas.sql
@@ -1,29 +1,8 @@
---
--- ER/Studio 8.0 SQL Code Generation
--- Company :      FI-UNAM
--- Project :      Proyecto.DM1
--- Author :       IsaacLopez117
---
--- Date Created : Saturday, December 04, 2021 17:51:46
--- Target DBMS : IBM DB2 UDB 8.x
---
-
--- 
--- TABLE: Categoria 
---
-
 CREATE TABLE Categoria(
     id_Categoria    SMALLINT       NOT NULL,
     tipo            VARCHAR(20)    NOT NULL,
     CONSTRAINT PK10 PRIMARY KEY (id_Categoria)
-)
-;
-
-
-
--- 
--- TABLE: Cliente 
---
+);
 
 CREATE TABLE Cliente(
     RFC           VARCHAR(13)    NOT NULL,
@@ -31,18 +10,12 @@ CREATE TABLE Cliente(
     ap_Paterno    VARCHAR(32)    NOT NULL,
     ap_Materno    VARCHAR(32),
     cp            SMALLINT       NOT NULL,
+    numero        SMALLINT       NOT NULL, 
     estado        VARCHAR(32)    NOT NULL,
     calle         VARCHAR(32)    NOT NULL,
     colonia       VARCHAR(32)    NOT NULL,
     CONSTRAINT PK2 PRIMARY KEY (RFC)
-)
-;
-
-
-
--- 
--- TABLE: Contiene 
---
+);
 
 CREATE TABLE Contiene(
     cod_Barras           INTEGER          NOT NULL,
@@ -50,27 +23,13 @@ CREATE TABLE Contiene(
     precio_Total_Art     DECIMAL(7, 2)    NOT NULL,
     cantidad_Articulo    INTEGER          NOT NULL,
     CONSTRAINT PK4 PRIMARY KEY (cod_Barras, id_Venta)
-)
-;
-
-
-
--- 
--- TABLE: Correo 
---
+);
 
 CREATE TABLE Correo(
     email    VARCHAR(64)    NOT NULL,
     RFC      VARCHAR(13)    NOT NULL,
     CONSTRAINT PK1 PRIMARY KEY (email)
-)
-;
-
-
-
--- 
--- TABLE: Guarda 
---
+);
 
 CREATE TABLE Guarda(
     id_Inventario    SMALLINT         NOT NULL,
@@ -79,27 +38,13 @@ CREATE TABLE Guarda(
     stock            INTEGER          NOT NULL,
     fecha_Compra     DATE             NOT NULL,
     CONSTRAINT PK12 PRIMARY KEY (id_Inventario, cod_Barras)
-)
-;
-
-
-
--- 
--- TABLE: Inventario 
---
+);
 
 CREATE TABLE Inventario(
     id_Inventario    SMALLINT       NOT NULL,
     nombre           VARCHAR(32),
     CONSTRAINT PK9 PRIMARY KEY (id_Inventario)
-)
-;
-
-
-
--- 
--- TABLE: Producto 
---
+);
 
 CREATE TABLE Producto(
     cod_Barras      INTEGER          NOT NULL,
@@ -108,14 +53,7 @@ CREATE TABLE Producto(
     descripcion     VARCHAR(50)      NOT NULL,
     id_Categoria    SMALLINT         NOT NULL,
     CONSTRAINT PK5 PRIMARY KEY (cod_Barras)
-)
-;
-
-
-
--- 
--- TABLE: Proveedor 
---
+);
 
 CREATE TABLE Proveedor(
     id_Proveedor    SMALLINT       NOT NULL,
@@ -127,40 +65,19 @@ CREATE TABLE Proveedor(
     cp              SMALLINT       NOT NULL,
     calle           VARCHAR(50)    NOT NULL,
     CONSTRAINT PK7 PRIMARY KEY (id_Proveedor)
-)
-;
-
-
-
--- 
--- TABLE: Surte 
---
+);
 
 CREATE TABLE Surte(
     id_Proveedor     SMALLINT    NOT NULL,
     id_Inventario    SMALLINT    NOT NULL,
     CONSTRAINT PK8 PRIMARY KEY (id_Proveedor, id_Inventario)
-)
-;
+);
 
-
-
--- 
--- TABLE: Telefeno 
---
-
-CREATE TABLE Telefeno(
+CREATE TABLE Telefono(
     num_Telefono    BIGINT      NOT NULL,
     id_Proveedor    SMALLINT    NOT NULL,
     CONSTRAINT PK6 PRIMARY KEY (num_Telefono)
-)
-;
-
-
-
--- 
--- TABLE: Venta 
---
+);
 
 CREATE TABLE Venta(
     id_Venta       INTEGER          NOT NULL,
@@ -168,14 +85,7 @@ CREATE TABLE Venta(
     pago_Final     DECIMAL(7, 2)    NOT NULL,
     RFC            VARCHAR(13)      NOT NULL,
     CONSTRAINT PK3 PRIMARY KEY (id_Venta)
-)
-;
-
-
-
--- 
--- TABLE: Contiene 
---
+);
 
 ALTER TABLE Contiene ADD CONSTRAINT RefVenta4 
     FOREIGN KEY (id_Venta)
@@ -187,20 +97,10 @@ ALTER TABLE Contiene ADD CONSTRAINT RefProducto5
     REFERENCES Producto(cod_Barras)
 ;
 
-
--- 
--- TABLE: Correo 
---
-
 ALTER TABLE Correo ADD CONSTRAINT RefCliente2 
     FOREIGN KEY (RFC)
     REFERENCES Cliente(RFC)
 ;
-
-
--- 
--- TABLE: Guarda 
---
 
 ALTER TABLE Guarda ADD CONSTRAINT RefInventario11 
     FOREIGN KEY (id_Inventario)
@@ -213,19 +113,10 @@ ALTER TABLE Guarda ADD CONSTRAINT RefProducto12
 ;
 
 
--- 
--- TABLE: Producto 
---
-
 ALTER TABLE Producto ADD CONSTRAINT RefCategoria10 
     FOREIGN KEY (id_Categoria)
     REFERENCES Categoria(id_Categoria)
 ;
-
-
--- 
--- TABLE: Surte 
---
 
 ALTER TABLE Surte ADD CONSTRAINT RefProveedor6 
     FOREIGN KEY (id_Proveedor)
@@ -237,24 +128,12 @@ ALTER TABLE Surte ADD CONSTRAINT RefInventario7
     REFERENCES Inventario(id_Inventario)
 ;
 
-
--- 
--- TABLE: Telefeno 
---
-
-ALTER TABLE Telefeno ADD CONSTRAINT RefProveedor8 
+ALTER TABLE Telefono ADD CONSTRAINT RefProveedor8 
     FOREIGN KEY (id_Proveedor)
     REFERENCES Proveedor(id_Proveedor)
 ;
-
-
--- 
--- TABLE: Venta 
---
 
 ALTER TABLE Venta ADD CONSTRAINT RefCliente3 
     FOREIGN KEY (RFC)
     REFERENCES Cliente(RFC)
 ;
-
-
--- BaseDeDatos/instrucciones.sql
+++ BaseDeDatos/instrucciones.sql
@@ -1,3 +1,84 @@
+--Retornar info del producto que haya menos de 3 en stock
+CREATE OR REPLACE FUNCTION menos_Thr() RETURNS TABLE (stock integer, marca varchar, descripcion varchar)
+AS $$
+DECLARE
+v_stock integer;
+nom_Marca varchar(120);
+BEGIN
+RETURN QUERY EXECUTE
+'SELECT stock, marca, descripcion FROM guarda LEFT JOIN producto
+ON guarda.cod_Barras=producto.cod_Barras 
+WHERE stock <= 3';
+END;
+$$
+LANGUAGE plpgsql;
+
+--Vista tipo factura
+CREATE VIEW FACTURA
+AS 
+SELECT Cliente.RFC, Cliente.nombre, Cliente.ap_Paterno, Cliente.ap_Materno, Cliente.cp, 
+Cliente.calle, Cliente.colonia, Venta.id_Venta, Venta.fecha_Venta, Venta.pago_Final, 
+Contiene.precio_Total_Art, Contiene.cantidad_Articulo, Producto.marca, Producto.descripcion FROM CLIENTE 
+INNER JOIN VENTA ON CLIENTE.RFC=VENTA.RFC INNER JOIN CONTIENE ON VENTA.id_Venta=CONTIENE.id_Venta 
+INNER JOIN PRODUCTO ON CONTIENE.cod_Barras=PRODUCTO.cod_Barras;
+
+--Indice de venta
+CREATE INDEX id_Vent ON VENTA(id_Venta);
+
+--Secuencia para id_Vent
+CREATE SEQUENCE Vent
+START WITH 0
+INCREMENT BY 1
+MINVALUE 0
+MAXVALUE 10
+CYCLE;
+
+--Modificaciones a la tabla venta
+ALTER TABLE Venta ALTER id_Venta SET DEFAULT NEXTVAL('Vent');
+ALTER TABLE Venta ALTER fecha_Venta SET DEFAULT current_date;
+
+--Procedimiento para verificar stock
+CREATE OR REPLACE FUNCTION existe() RETURNS TRIGGER AS $existe$
+DECLARE decremento integer;
+BEGIN 
+decremento= new.cantidad_Articulo;
+IF (select stock FROM guarda WHERE cod_Barras = NEW.cod_Barras) < decremento THEN 
+raise notice 'No hay suficiente producto';
+ROLLBACK; 
+RETURN stock;
+END IF;
+IF (select stock FROM guarda where cod_Barras = NEW.cod_Barras ) >= decremento THEN
+UPDATE guarda SET stock= ((SELECT stock FROM guarda WHERE cod_Barras=             NEW.cod_Barras) - decremento) WHERE cod_Barras = NEW.cod_Barras;
+IF (select stock FROM guarda where  cod_Barras = NEW.cod_Barras ) <= 3 THEN
+raise notice 'stock actualizado, REVISE SU INVENTARIO';
+END IF;
+RETURN NULL;
+END IF; 
+RETURN NULL;
+END;
+$existe$ LANGUAGE plpgsql;
+
+CREATE TRIGGER existe AFTER INSERT
+ON CONTIENE FOR EACH ROW
+EXECUTE PROCEDURE existe();
+
+--FUNTCION para la utilidad de cada producto
+CREATE OR REPLACE FUNCTION retorna_Utilidad(codigo INT)RETURNS DECIMAL(7,2) AS
+$$
+DECLARE
+v_Cod_Barras INT;
+v_Utilidad DECIMAL(7,2);
+BEGIN
+SELECT Guarda.cod_Barras, SUM(precio-precio_Compra) INTO v_Cod_Barras, v_Utilidad FROM Guarda 
+INNER JOIN Producto
+ON Producto.cod_Barras = Guarda.cod_Barras
+WHERE Guarda.cod_Barras = codigo
+GROUP BY Guarda.cod_Barras;
+RETURN v_Utilidad;
+END;
+$$
+LANGUAGE plpgsql;
+
 --FUNCTION para retornar los pagos finales
 CREATE OR REPLACE FUNCTION retorna_Pago_Final(fecha DATE)RETURNS DECIMAL(7,2) AS
 $$
@@ -13,8 +94,41 @@ END;
 $$
 LANGUAGE plpgsql;
 
---FUNCTION para retornar los precios por producto
+--FUNCTION para retornar los pagos finales entre fechas
+CREATE OR REPLACE FUNCTION retorna_Pago_Final(fecha_inicio DATE, fecha_fin DATE)RETURNS DECIMAL(7,2) AS
+$$
+DECLARE
+v_Pago DECIMAL(7,2);
+BEGIN
+SELECT SUM(pago_Final) INTO v_Pago FROM Venta
+WHERE fecha_Venta BETWEEN fecha_inicio AND fecha_fin;
+RETURN v_Pago;
+END;
+$$
+LANGUAGE plpgsql;
+
+--FUNCTION para retornar las ganancias
 CREATE OR REPLACE FUNCTION retorna_Pago_Ganancia(fecha DATE)RETURNS DECIMAL(7,2) AS
+$$
+DECLARE
+v_Pago DECIMAL(7,2);
+v_Bruto DECIMAL(7,2);
+v_Ganancia DECIMAL(7,2);
+BEGIN
+SELECT SUM(pago_Final), SUM(precio_Compra*cantidad_articulo) 
+INTO v_Pago, v_Bruto FROM Venta 
+INNER JOIN (Contiene INNER JOIN (Producto INNER JOIN Guarda ON Producto.cod_Barras = Guarda.cod_Barras)
+ON Contiene.cod_Barras = Producto.cod_Barras)
+ON Venta.id_Venta = Contiene.id_Venta
+WHERE Venta.fecha_venta = fecha;
+v_Ganancia = v_Pago - v_Bruto;
+RETURN v_Ganancia;
+END;
+$$
+LANGUAGE plpgsql;
+
+--FUNCTION para retornar las ganancias por fechas
+CREATE OR REPLACE FUNCTION retorna_Pago_Ganancia(fecha_inicio DATE, fecha_fin DATE)RETURNS DECIMAL(7,2) AS
 $$
 DECLARE
 v_Pago DECIMAL(7,2);
@@ -28,14 +142,26 @@ INNER JOIN (Producto
 INNER JOIN Guarda ON Producto.cod_Barras = Guarda.cod_Barras)
 ON Contiene.cod_Barras = Producto.cod_Barras)
 ON Venta.id_Venta = Contiene.id_Venta
-WHERE Venta.fecha_venta = fecha;
+WHERE Venta.fecha_Venta BETWEEN fecha_inicio AND fecha_fin;
 v_Ganancia = v_Pago - v_Bruto;
 RETURN v_Ganancia;
 END;
 $$
 LANGUAGE plpgsql;
 
-
 --Llamada de funciones
+SELECT retorna_Utilidad(53204);
+
+SELECT menos_Thr();
+
+SELECT * FROM FACTURA WHERE RFC = 'GEC85014014I2';
+
 SELECT retorna_Pago_Final('2021-10-24');
+SELECT retorna_Pago_Final('2021-10-20','2021-11-30');
+
 SELECT retorna_Pago_Ganancia('2021-11-24');
+SELECT retorna_Pago_Ganancia('2021-10-20','2021-10-30');
+
+INSERT INTO CONTIENE VALUES (53205,004,9500.50,3);
+
+INSERT INTO Venta(pago_final,RFC) VALUES(9500.50, 'GEC85014014I1');
